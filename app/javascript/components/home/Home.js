1b5052426ff0c8ab66d172c8572310580ab49e06
imposttor <i.v.metelev99@gmail.com>
2021-11-07 19:10:53 +0600

    init project

---
+++ app/javascript/components/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Home extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+      <div>
+        TEST
+      </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home

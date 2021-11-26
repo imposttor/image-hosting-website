55d036dd52a4a23f17e3ae45bb01ed839dda3f4c
imposttor <i.v.metelev99@gmail.com>
2021-11-27 03:39:48 +0600

    Add react components. Share component to share posts on social networks.

--- app/javascript/components/posts/show/Show.js
+++ app/javascript/components/posts/show/Show.js
@@ -4,12 +4,25 @@ class Show extends React.Component {
   render () {
     return (
         <React.Fragment>
-        <div className="main">
-            TEST
+        <div className="header">
+            <div className="header1">
+                <a href={this.props.feedLink}>Feed</a>
+            </div>
+            <div className="header2">
+                <a data-method="DELETE" href={this.props.signOutLink}>Back</a>
+            </div>
+        </div>
+        <div className="imagesBlock">
+            <img className = "images" src={this.props.imageLink}/>
         </div>
       </React.Fragment>
     );
   }
 }
-
+Show.propTypes = {
+    imageLink: PropTypes.string,
+    feedLink: PropTypes.string,
+    signOutLink: PropTypes.string,
+    backLink: PropTypes.string
+};
 export default Show
--- app/javascript/components/share/Share.js
+++ app/javascript/components/share/Share.js
@@ -1,12 +1,63 @@
 import React from "react"
 import PropTypes from "prop-types"
+import {
+    EmailShareButton,
+      FacebookShareButton,
+      TelegramShareButton,
+      VKShareButton,
+      WhatsappShareButton,
+      EmailIcon,
+      FacebookIcon,
+      TelegramIcon,
+      VKIcon,
+      WhatsappIcon,
+} from "react-share";
 class Share extends React.Component {
-  render () {
+  render() {
     return (
       <React.Fragment>
-      <div className="main">
-          TEST
+      <div className ="ShareBlock">
+      <div className="shareButtons">
+        <EmailShareButton
+          url={"shareUrl"}
+          title={"title"}
+        >
+          <EmailIcon size={32} round />
+        </EmailShareButton>
       </div>
+      <div className="shareButtons">
+         <VKShareButton
+           url={"shareUrl"}
+           title={"title"}
+         >
+           <VKIcon size={32} round />
+         </VKShareButton>
+         </div>
+         <div className="shareButtons">
+          <WhatsappShareButton
+            url={"shareUrl"}
+            title={"title"}
+          >
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
+        </div>
+        <div className="shareButtons">
+          <FacebookShareButton
+            url={"shareUrl"}
+            title={"title"}
+          >
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+        </div>
+        <div className="shareButtons">
+          <TelegramShareButton
+            url={"shareUrl"}
+            title={"title"}
+          >
+            <TelegramIcon size={32} round />
+          </TelegramShareButton>
+        </div>
+        </div>
       </React.Fragment>
     );
   }
